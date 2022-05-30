897c3a34bc0427500efe50ad270d984cd345b9c1
JoonHwan_Jeon <teiresias1987@gmail.com>
2022-05-30 13:01:36 +0900

    Initial Commit

---
+++ ClimbingDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClimbingDiary
+//
+//  Created by Joonhwan Jeon on 2022/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
